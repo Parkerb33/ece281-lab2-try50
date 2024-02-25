19ca308695fe3347ea64ef983364ac71ccfbbae5
parkerb33 < 142423100+Parkerb33@users.noreply.github.com>
2024-02-25 15:13:20 -0700

    final product :)

---
+++ src/hdl/sevenSegDecoder.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/25/2024 11:39:22 AM
+-- Design Name: 
+-- Module Name: sevenSegDecoder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sevenSegDecoder is
+    Port ( i_D : in STD_LOGIC_VECTOR (3 downto 0);
+           o_S : in STD_LOGIC_VECTOR (6 downto 0));
+end sevenSegDecoder;
+
+architecture Behavioral of sevenSegDecoder is
+
+begin
+
+
+end Behavioral;
